d1bd7fa5af20cfc5890f12b10a8cd87a65af3b99
AntonioVaillo <antoniojvaillo@hotmail.com>
2020-01-31 14:13:52 +0100

    Algunos comentarios en código

--- js/tarea5.js
+++ js/tarea5.js
@@ -1,5 +1,7 @@
+
 $(document).ready(function() {
 
+    //Controlamos a través de la clase .close el cierre del div del detalle del curriculum
     $('.close').click(function() {
         $(this).parent('div').parent('div').collapse('toggle');
     });
