d27440f329f163cfb1b134a587e6bdbfe12f10e5
AntonioVaillo <antoniojvaillo@hotmail.com>
2020-02-03 12:06:11 +0100

    Cambio de jquery y mejora en el scroll a las secciones

--- js/tarea5.js
+++ js/tarea5.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
     $('.close').click(function() {
         $(this).parent('div').parent('div').collapse('toggle');
     });
+
+    
+});
+
+
+//Funcion que permite desplazar el scroll después de hacer click en el menú
+//Así evitamos que se solape el menú y se vea correctamente la sección correspondiente
+$('.nav-link').click(function() {
+    
+    var y = $(window).scrollTop();  //Current position at page
+    var id = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: ($(id).offset().top) - 60
+    }, 600);
+
 });
